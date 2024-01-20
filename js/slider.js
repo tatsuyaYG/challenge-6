eede3604053a9fb9a2a20d774c497b892ff18a3b
tatsuyaYAGI <yagiappwp@gmail.com>
2024-01-21 00:26:28 +0900

    slider実装

---
+++ js/slider.js
@@ -0,0 +1,53 @@
+// メインビジュアル
+$(function () {
+  $(".p-slider--main").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    // centerMode: true,
+    centerPadding: "30px",
+    dots: true,
+    slidesToShow: 1,
+    speed: 1500,
+  });
+});
+$(function () {
+  $(".slider-track")
+    .on("init", function (event, slick) {
+      $(this).append('<div class="slick-num"><span class="now-count"></span> / <span class="all-count"></span></div>');
+      $(".now-count").text(slick.currentSlide + 1); // 現在のスライド番号(+1が無いと0からスタートしてしまう)
+      $(".all-count").text(slick.slideCount); // スライドの総数
+    })
+    .slick({
+      // 通常のスライダー同様、オプションを記入
+    })
+    .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+      $(".now-count").text(nextSlide + 1); // 現在のスライド番号の次のスライドになったら番号を+1
+    });
+});
+
+$(function () {
+  $(".p-slider--about").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    // centerMode: true,
+    centerPadding: "30px",
+    dots: true,
+    slidesToShow: 3,
+    speed: 8000,
+  });
+});
+
+$(function () {
+  $(".p-slider--gallery").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    centerPadding: "200px",
+    dots: true,
+    slidesToShow: 2,
+    speed: 8000,
+  });
+});
