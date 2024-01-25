4e45921a1efa5e805a5690c301daa5673753407b
tatsuyaYAGI <yagiappwp@gmail.com>
2024-01-26 00:00:49 +0900

    スライド数表示

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,20 @@
 // メインビジュアル
 $(function () {
-  $(".p-slider--main").slick({
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    // centerMode: true,
-    centerPadding: "30px",
-    dots: true,
-    slidesToShow: 1,
-    speed: 1500,
-  });
-});
-$(function () {
-  $(".slider-track")
+  $(".p-slider--main")
     .on("init", function (event, slick) {
-      $(this).append('<div class="slick-num"><span class="now-count"></span> / <span class="all-count"></span></div>');
+      $(this).append('<div class="p-slider--num"><span class="now-count"></span> <span></span> <span class="all-count"></span></div>');
       $(".now-count").text(slick.currentSlide + 1); // 現在のスライド番号(+1が無いと0からスタートしてしまう)
       $(".all-count").text(slick.slideCount); // スライドの総数
     })
     .slick({
       // 通常のスライダー同様、オプションを記入
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: false,
+      slidesToShow: 1,
+      speed: 1500,
+
     })
     .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
       $(".now-count").text(nextSlide + 1); // 現在のスライド番号の次のスライドになったら番号を+1
@@ -33,7 +28,7 @@ $(function () {
     autoplaySpeed: 0,
     // centerMode: true,
     centerPadding: "30px",
-    dots: true,
+    dots: false,
     slidesToShow: 3,
     speed: 8000,
   });
@@ -46,7 +41,7 @@ $(function () {
     autoplaySpeed: 0,
     centerMode: true,
     centerPadding: "200px",
-    dots: true,
+    dots: false,
     slidesToShow: 2,
     speed: 8000,
     responsive: [
